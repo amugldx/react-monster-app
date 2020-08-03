8b50288290b560412db0912f7f00622627cde518
Ammar Mughal <meet.sagi.here@gmail.com>
2020-08-03 12:26:42 +0500

    Updates

---
+++ precache-manifest.dcb7faa4bf1da06881fd9a359f5bc389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2494276c54b4f9b683ae200a2920c24",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "b6c576b1c39955306b76",
+    "url": "/react-monster-app/static/css/main.f484f13d.chunk.css"
+  },
+  {
+    "revision": "0d79a8978a29184912dc",
+    "url": "/react-monster-app/static/js/2.9948f0c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-app/static/js/2.9948f0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c576b1c39955306b76",
+    "url": "/react-monster-app/static/js/main.7988d3c2.chunk.js"
+  },
+  {
+    "revision": "d00d8a8cdeb17d0931fb",
+    "url": "/react-monster-app/static/js/runtime-main.bb9078f6.js"
+  }
+]);
